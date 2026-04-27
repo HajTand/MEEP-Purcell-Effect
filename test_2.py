7a574120b243e6bf310bd7f00141c04ceba2d1f1
HajTand <volkovnikita3223@ya.ru>
2026-04-28 00:02:43 +0300

    new comm

--- test_2.py
+++ test_2.py
@@ -24,6 +24,34 @@ dpml = 1.0
 df = 0.2 * dipole_vac_freq
 
 "sz - размер рабочей области целого вместе с pml"
-sz = 10 + 2 * dpml
+sz = 10.0 + 2.0 * dpml
 cell_size = mp.Vector3(z=sz)
+"размер ячейки"
+pml_layers = [mp.PML(dpml)]
+"количество слоев pml"
 
+"задаем источник и его параметры, центр геометрический"
+"ширины df источник"
+sources = [ 
+    mp.Source(
+        mp.GaussianSource(dipole_vac_freq, fwidth=df),
+        component=mp.Ex,
+        center=mp.Vector3(z=-0.5 * sz + dpml),
+    )
+]
+
+"задаем симуляцию, dim - размерность моделирования"
+"размер ячейки - cell_size, исчтоник - sources, разрешение - resolution"
+"boundary layers - граничные слои"
+sim = mp.Simulation(
+    cell_size=cell_size,
+    boundary_layers=pml_layers,
+    sources=sources,
+    dimensions=1,
+    resolution=resolution,
+)
+
+sim.run(mp.dft_ldos(dipole_vac_freq, df, 1),
+            until_after_sources=mp.stop_when_fields_decayed(50, mp.Ex, mp.Vector3(), 1e-6))
+ldos_sim0 = sim.ldos_data[0]
+print(ldos_sim0)
