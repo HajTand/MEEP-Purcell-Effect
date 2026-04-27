0c45feae01e574cc5fc085e5ff0279260d53b44c
HajTand <volkovnikita3223@ya.ru>
2026-04-28 01:46:45 +0300

    add modeling

--- test_2.py
+++ test_2.py
@@ -9,6 +9,8 @@ import meep as mp
 resolution = 200  # pixels/μm
 "показатель преломления среды"
 n = 2.4
+"диэлектрическая проницаемость среды"
+epsilon = n**2
 
 "длина волны и частота диполя в вакууме"
 dipole_vac_wavelen = 1.0    #μm
@@ -22,9 +24,15 @@ dpml = 1.0
 "Достаточно узкий, чтобы не возбуждать далекие частоты (точность)"
 "Ширина испульса диполя"
 df = 0.2 * dipole_vac_freq
+nfreq = 1
 
+
+# расчет без зеркал, просто однородная среда
+
+"L - размер среды"
+L = 10.0
 "sz - размер рабочей области целого вместе с pml"
-sz = 10.0 + 2.0 * dpml
+sz = L + 2.0 * dpml
 cell_size = mp.Vector3(z=sz)
 "размер ячейки"
 pml_layers = [mp.PML(dpml)]
@@ -36,7 +44,7 @@ sources = [
     mp.Source(
         mp.GaussianSource(dipole_vac_freq, fwidth=df),
         component=mp.Ex,
-        center=mp.Vector3(z=-0.5 * sz + dpml),
+        center=mp.Vector3(z=0),
     )
 ]
 
@@ -49,9 +57,67 @@ sim = mp.Simulation(
     sources=sources,
     dimensions=1,
     resolution=resolution,
+    default_material=mp.Medium(index=n)
 )
 
-sim.run(mp.dft_ldos(dipole_vac_freq, df, 1),
-            until_after_sources=mp.stop_when_fields_decayed(50, mp.Ex, mp.Vector3(), 1e-6))
-ldos_sim0 = sim.ldos_data[0]
-print(ldos_sim0)
+"запуск симуляции, задаем когда закончится симуляция"
+"каждые 50 фемтосекунд после того как закончился импульс света"
+"компонента Ex должно стать 1e-9"
+sim.run(mp.dft_ldos(dipole_vac_freq, df, nfreq),
+        until_after_sources=mp.stop_when_fields_decayed(50, mp.Ex, mp.Vector3(), 1e-9))
+
+ldos_bulk = sim.ldos_data[0]
+print(f"LDOS_bulk = {ldos_bulk:.6f}")
+sim.reset_meep()
+# рассчет с резонатором
+f_P = []
+Norm_Thickness = np.arange(0.2,3.2,0.2)
+
+for value in Norm_Thickness:
+    sz = ((value * dipole_vac_wavelen) / n)
+    cell_size = mp.Vector3(z = sz)
+    sources = [ 
+        mp.Source(
+            mp.GaussianSource(dipole_vac_freq, fwidth=df),
+            component=mp.Ex,
+            center=mp.Vector3(z=0),
+        )
+    ]
+    
+    sim = mp.Simulation(
+       cell_size=cell_size,
+       sources=sources,
+       dimensions=1,
+       resolution=resolution,
+       default_material=mp.Medium(index=n)
+       )
+       # boundary_layers НЕТ! потому что границы = PEC зеркала
+    
+    sim.run(mp.dft_ldos(dipole_vac_freq, df, nfreq),
+            until_after_sources=10000)
+
+    ldos_res = sim.ldos_data[0]
+    f_P.append(ldos_res/ldos_bulk)
+    sim.reset_meep()
+
+#теория
+def purcell_theory(x):
+    """
+    Аналитическая формула из IEEE J. Quantum Electronics, Vol. 34, pp. 71-76 (1998)
+    x = nL/λ  — нормированная толщина резонатора
+    """
+    m = np.floor(x + 0.5)  # номер моды
+    term1 = (3 * m) / (4 * x)
+    term2 = (4 * m**3 - m) / (16 * x**3)
+    return term1 + term2
+
+# Рассчитываем теоретические значения для тех же точек
+purcell_theory_vals = [purcell_theory(x) for x in Norm_Thickness] 
+   
+print()
+plt.plot(Norm_Thickness,f_P)
+plt.plot(Norm_Thickness,purcell_theory_vals)
+
+print(ldos_bulk)
+
+
